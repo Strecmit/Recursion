2b5a70c4e507fccf32b8f85988598bc3d6c24a2d
Victor <strecmit@gmail.com>
2023-12-27 17:56:45 +0200

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myMassive = new int[size];
+
+
+//-------
+int[] createMassive(int[] array, int i)
+{
+    array[i] = new Random().Next(1,100);
+    if(i < array.Length){
+        i++;
+        createMassive(array, i);
+    }
+    else {return array;}
+
+}
+
+myMassive = createMassive(myMassive, 0);
+Console.Write(myMassive);
+
+
