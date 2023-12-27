57c3da84b3180852a56f0e991da9aa5648c1e98b
Victor <strecmit@gmail.com>
2023-12-27 17:03:36 +0200

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Вычисление функции Акермана: А(n,m).\r\nВведите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\r\nВычисленный результат равен {Ackermann(n,m)}");
+
+int Ackermann(int n, int m){
+    //Console.WriteLine($"m={m}, n={n} ");
+    if(n == 0) {return m+1;}
+    else if(m == 0) {return Ackermann(n-1, 1);}
+    else {return Ackermann(n-1,Ackermann(n,m-1));}
+    
+}
