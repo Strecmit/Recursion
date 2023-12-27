ff91101e0cbdded94676a792aba2d4821be80ebe
Victor <strecmit@gmail.com>
2023-12-27 16:07:48 +0200

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите два положительных числа. Первое число начало интервала: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число конец интервала: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+sumInterval(m, n);
+
+//------------
+void sumInterval (int start, int end){
+    
+    if (start > end){
+        return;
+    }
+    else {
+        Console.Write(start + " ");
+        start++;
+        sumInterval(start, end);
+    }
+
+
+
+}
