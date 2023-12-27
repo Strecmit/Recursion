678ff1a27e56287e06988a457e4b6c7ec385fb9f
Victor <strecmit@gmail.com>
2023-12-27 19:14:30 +0200

    task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,19 +3,41 @@ int size = Convert.ToInt32(Console.ReadLine());
 int[] myMassive = new int[size];
 
 
-//-------
-int[] createMassive(int[] array, int i)
+myMassive = createMassive(myMassive, 0);
+Console.WriteLine($"Массив:       [{String.Join(" ", myMassive)}]");
+Console.WriteLine($"Новый массив: [{String.Join(" ", newMassive(myMassive, 0))}]");
+
+
+//-------Выводим массив начиная с последнего элемента
+
+int[] newMassive(int[] array, int i)
 {
-    array[i] = new Random().Next(1,100);
-    if(i < array.Length){
+    int length = array.Length;
+    if (i < (length / 2))
+    {
+        int temp = array[i];
+        array[i] = array[(length - 1) - i];
+        array[(length - 1) - i] = temp;
         i++;
-        createMassive(array, i);
+        newMassive(array, i);
     }
-    else {return array;}
+    return array;
 
 }
 
-myMassive = createMassive(myMassive, 0);
-Console.Write(myMassive);
+
+//-------Создаём массив заданного размера
+int[] createMassive(int[] array, int i)
+{
+    array[i] = new Random().Next(1, 100);
+    if (i < array.Length - 1)
+    {
+        i++;
+        createMassive(array, i);
+    }
+    return array;
+
+}
+
 
 
